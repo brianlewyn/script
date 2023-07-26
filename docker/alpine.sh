5ea4584593062eda26f344ac910be8828f4ed2df
brianlewyn <brianlewyn@gmail.com>
2023-07-25 18:54:52 -0600

    feat(docker): add a file to configure alpine linux

---
+++ docker/alpine.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+USERNAME="brianlewyn"
+GITHUB=$USERNAME
+
+# Update & Upgrade
+apk update && apk upgrade
+
+# Nano
+apk add nano
+
+# Vim
+apk add vim
+echo "set number" > ~/.vimrc
+echo "set tabstop=4" >> ~/.vimrc
+
+# Neovim
+apk add neovim
+
+# Git
+apk add git
+git config --global user.name $GITHUB
+git config --global user.email $GITHUB@gmail.com
+git config --global init.defaultBranch main
+git config --global credential.helper store
+git config --global core.editor nvim
+git config --global core.pager cat
+
+# Bash
+apk add bash
+
+# Oh-My-Bash
+git clone https://github.com/ohmybash/oh-my-bash.git ~/.oh-my-bash
+cd ~/.oh-my-bash/themes && mv rr .rr && rm -rf * && mv .rr rr
+sed -i "18s/}.*\${/}$USERNAME\${/" ~/.oh-my-bash/themes/rr/rr.theme.sh
+sed -i '27s/\(${.*} \) \(${.*} \)\$/\2\1$/' ~/.oh-my-bash/themes/rr/rr.theme.sh
+sed -i '36s/}/  PS1+="\\n$ "\n}/' ~/.oh-my-bash/themes/rr/rr.theme.sh
+cp ~/.oh-my-bash/templates/bashrc.osh-template ~/.bashrc
+sed -i '12s/font/rr/' ~/.bashrc
---
+++ docker/docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# save this file as ~/docker.sh 
+# add the alias to ~/.bashrc
+# alias image="~/./docker.sh"
+
+ARG=$1
+NAME=$2
+
+if [ -z $ARG ]; then 
+	echo 'ARG?'
+	exit
+fi
+
+if [ -z $NAME ]; then 
+	echo 'NAME?'
+	exit
+fi
+
+if [ "$ARG" == "add" ]; then
+	docker build -t $NAME .
+	docker run -dit --name "$NAME-dev" -v .:/app $NAME
+	docker start -i "$NAME-dev"
+	exit
+fi
+
+if [ "$ARG" == "rm" ]; then
+	docker rm "$NAME-dev"
+	docker rmi $NAME
+	exit
+fi
