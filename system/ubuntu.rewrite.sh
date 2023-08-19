8154f7b00b3853083a99720cc53137de65a87b5a
Brayan Mejía <94902875+brianlewyn@users.noreply.github.com>
2023-08-19 15:06:46 -0600

    feat(ubuntu): add note for download

--- system/ubuntu.rewrite.sh
+++ system/ubuntu.rewrite.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/brianlewyn/script/main/system/ubuntu.rewrite.sh)"
+
 # Should Be Main Server
 # chmod +x ubuntu.sh
 
@@ -30,7 +32,7 @@ sudo apt install keepassxc
 
 # Docker (Download DEB package https://docs.docker.com/desktop/install/ubuntu/)
 sudo apt-get update
-sudo apt-get install ca-certificates gnupg
+sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
